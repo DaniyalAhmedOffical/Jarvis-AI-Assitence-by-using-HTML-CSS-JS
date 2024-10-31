f8488988344318d06c087b35a18ffb05a7c65f25
Daniyal Ahmed <64687172+DaniyalAhmedOffical@users.noreply.github.com>
2024-11-01 00:49:30 +0500

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@ function wishMe(){
     var hour = day.getHours();
 
     if(hour>=0 && hour<12){
-        speak("Good Morning Daniyal Sir kesy ho...")
+        speak("Good Morning Daniyal Sir ...")
     }
 
     else if(hour>12 && hour<17){
