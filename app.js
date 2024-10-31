716a257272968344d6d4ef1cfc9827738c21fd3e
Daniyal Ahmed <64687172+DaniyalAhmedOffical@users.noreply.github.com>
2024-11-01 01:15:59 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,67 +1,27 @@
-const btn = document.querySelector('.talk')
-const content = document.querySelector('.content')
-
-
-function speak(text){
-    const text_speak = new SpeechSynthesisUtterance(text);
-
-    text_speak.rate = 1;
-    text_speak.volume = 1;
-    text_speak.pitch = 1;
-
-    window.speechSynthesis.speak(text_speak);
-}
-
-function wishMe(){
-    var day = new Date();
-    var hour = day.getHours();
-
-    if(hour>=0 && hour<12){
-        speak("Good Morning Daniyal Sir ...")
-    }
-
-    else if(hour>12 && hour<17){
-        speak("Good Afternoon Daniyal Sir...")
-    }
-
-    else{
-        speak("Good Evenining Daniyal Sir...")
-    }
-
-}
-
-window.addEventListener('load', ()=>{
-    speak("Initializing JARVIS..");
-    wishMe();
-});
-
-const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
-
-const recognition =  new SpeechRecognition();
-
-recognition.onresult = (event)=>{
-    const currentIndex = event.resultIndex;
-    const transcript = event.results[currentIndex][0].transcript;
-    content.textContent = transcript;
-    takeCommand(transcript.toLowerCase());
-
-}
-
-btn.addEventListener('click', ()=>{
-    content.textContent = "Listening...."
-    recognition.start();
-})
-
-
 
 function takeCommand(message){
     if(message.includes('hey') || message.includes('hello')){
-        speak("Hello Sir, How May I Help You?");
+        // speak("Hello Sir, How May I Help You?");
+        speak("Hello Sir, my name is Jarvis. I was created by Mr. Daniyal, and I am an AI assistant. How may I help you?");
+
+    }
+
+    else if(message.includes("dani")){
+        //window.open("https://google.com", "_blank");
+        speak("g dani bolo")
+
+    }
+    else if(message.includes("wish")){
+        //window.open("https://google.com", "_blank");
+        speak("Warisha Daniyal LOVE YOU. AP UN KI JAN HA AP MAI JAAN BASTI HA DANIYAL SIR KI WO AP SY BHT PYAR KRTY HAI AP UN K LIYE EK CHAND KA TUKRA HA AP BHT LUCKY HAI K AP KO MERY SIR BHT PYAR KRTY HAI I LOVE YOU MERI JAAN YE DANIYAL SIR KI TARAH SY AP K LIYE WARISHA DANIYAL AHMED ")
+
+        
     }
     else if(message.includes("open google")){
         window.open("https://google.com", "_blank");
         speak("Opening Google...")
     }
+
     else if(message.includes("open youtube")){
         window.open("https://www.youtube.com/", "_blank");
         speak("Opening Youtube...")
