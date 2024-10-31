51cb0ddbe2487c370499d6b234ef23abb76bfb89
Daniyal Ahmed <64687172+DaniyalAhmedOffical@users.noreply.github.com>
2024-11-01 01:40:30 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,3 +1,58 @@
+const btn = document.querySelector('.talk')
+const content = document.querySelector('.content')
+
+
+function speak(text){
+    const text_speak = new SpeechSynthesisUtterance(text);
+
+    text_speak.rate = 1;
+    text_speak.volume = 1;
+    text_speak.pitch = 1;
+
+    window.speechSynthesis.speak(text_speak);
+}
+
+function wishMe(){
+    var day = new Date();
+    var hour = day.getHours();
+
+    if(hour>=0 && hour<12){
+        speak("Good Morning Daniyal Sir kesy ho...")
+    }
+
+    else if(hour>12 && hour<17){
+        speak("Good Afternoon Daniyal Sir...")
+    }
+
+    else{
+        speak("Good Evenining Daniyal Sir...")
+    }
+
+}
+
+window.addEventListener('load', ()=>{
+    speak("Initializing JARVIS..");
+    wishMe();
+});
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition =  new SpeechRecognition();
+
+recognition.onresult = (event)=>{
+    const currentIndex = event.resultIndex;
+    const transcript = event.results[currentIndex][0].transcript;
+    content.textContent = transcript;
+    takeCommand(transcript.toLowerCase());
+
+}
+
+btn.addEventListener('click', ()=>{
+    content.textContent = "Listening...."
+    recognition.start();
+})
+
+
 
 function takeCommand(message){
     if(message.includes('hey') || message.includes('hello')){
@@ -11,9 +66,10 @@ function takeCommand(message){
         speak("g dani bolo")
 
     }
-    else if(message.includes("wish")){
+    else if(message.includes("da")){
         //window.open("https://google.com", "_blank");
-        speak("Warisha Daniyal LOVE YOU. AP UN KI JAN HA AP MAI JAAN BASTI HA DANIYAL SIR KI WO AP SY BHT PYAR KRTY HAI AP UN K LIYE EK CHAND KA TUKRA HA AP BHT LUCKY HAI K AP KO MERY SIR BHT PYAR KRTY HAI I LOVE YOU MERI JAAN YE DANIYAL SIR KI TARAH SY AP K LIYE WARISHA DANIYAL AHMED ")
+       // speak("Warisha Daniyal LOVE YOU. AP UN KI JAN HA AP MAI JAAN BASTI HA DANIYAL SIR KI WO AP SY BHT PYAR KRTY HAI AP UN K LIYE EK CHAND KA TUKRA HA AP BHT LUCKY HAI K AP KO MERY SIR BHT PYAR KRTY HAI I LOVE YOU MERI JAAN YE DANIYAL SIR KI TARAH SY AP K LIYE WARISHA DANIYAL AHMED ")
+       speak("Warisha, Daniyal loves you. You are his life, and his heart belongs to you. He loves you deeply, and to him, you are like a precious piece of the moon. You are very lucky to have my sir's love. I love you, my dear my princess my queen my wife. This message is from Daniyal Sir to you, Warisha Daniyal Ahmed.");
 
         
     }
